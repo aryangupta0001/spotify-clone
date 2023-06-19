8ff8be916461117c2a768b78d7c7f33572d8cae9
Aryan Gupta <aryanguptabareilly067@gmail.com>
2023-06-19 21:04:12 +0530

    ddeleted js

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-function hover_white(){
-    let target = document.getElementById("upper-left-sec");
-    target = target.getElementsByTagName("div")[1];
-    target.getElementsByTagName("svg")[0].style.fill = "white";
-    target.getElementsByTagName("span")[0].style.color = "white";
-
-}
-
